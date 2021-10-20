b29f75f4611db736f2598797f3f25159328122a4
Ritoshree <ritashree2001.roy@gmail.com>
2021-10-20 19:18:53 +0530

    adding biodata

--- Bce.java
+++
@@ -1,7 +0,0 @@
-class Bce
-{
-public static void main(String args[])
-{
-System.out.println("WELCOME TO JAVA WORLD");
-}
-}
--- Bcet.java
+++
@@ -1,7 +0,0 @@
-class Bcet 
-{
-public static void main(String args[])
-{
-System.out.println("RITOSHREE ROY");
-}
-}
